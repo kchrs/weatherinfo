eaa05d134c4130db9b7edd312f6a1489726daef7
kchristopoulos <kchristopoulos@kchristopouloss-Mac-mini.local>
2019-04-11 16:12:37 +0300

    add geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -41,6 +41,32 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+
+
 };
+
+
+// onSuccess Callback
+    //   This method accepts a `Position` object, which contains
+    //   the current GPS coordinates
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                            'Longitude: ' + position.coords.longitude     + '<br />' +
+                            '<hr />'      + element.innerHTML;
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    // Options: throw an error if no update is received every 30 seconds.
+    //
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+
 
 app.initialize();
